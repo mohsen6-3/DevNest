4c5bf993fe4b64c8e418fd8d7005d3c1904ffc02
Mohsen6_3 <Mohsen@gmail.com>
2026-04-30 11:04:05 +0300

    Added posts model

--- DevNest/posts/models.py
+++ DevNest/posts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class PostType(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    def __str__(self):
+        return self.name
+    
+
+    
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    post_type = models.ForeignKey(PostType, on_delete=models.CASCADE,default=None, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.created_at}'
+
--- DevNest/posts/views.py
+++ DevNest/posts/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from .models import Post
 # Create your views here.
-def post_list_view(request: HttpRequest):
+def post_create_view(request: HttpRequest):
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        detail = request.POST.get('detail')
+        posts = Post(title=title, detail=detail)
+        posts.save()
     
-    return render(request, 'posts/Q&A_Form.html')
+    return render(request, 'posts/posts_page.html')
+
+def post_list_view(request: HttpRequest):
+    posts = Post.objects.all()
+    return render(request, 'posts/posts_page.html', {'posts': posts})
