4cb79b5e2a2b7cca234cab21c14e74a89bc0db07
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2026-05-04 11:47:45 +0300

    added external db

--- DevNest/DevNest/settings.py
+++ DevNest/DevNest/settings.py
@@ -89,9 +89,7 @@ WSGI_APPLICATION = 'DevNest.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
