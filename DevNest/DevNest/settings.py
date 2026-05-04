269dede38558091be5bcf0b8816ba65fe48c8bea
Mohammed Alqadda <MohammedFAlqadda@gmail.com>
2026-05-04 04:52:52 +0300

    try to deploy

--- DevNest/DevNest/settings.py
+++ DevNest/DevNest/settings.py
@@ -147,3 +147,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+SITE_URL = os.getenv('SITE_URL', 'https://devnest-lj72.onrender.com')
