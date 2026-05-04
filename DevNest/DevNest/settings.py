354ca738c868c7bc1d224778833af1a21eda20c9
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2026-05-04 11:43:24 +0300

    added external db

--- DevNest/DevNest/settings.py
+++ DevNest/DevNest/settings.py
@@ -88,10 +88,11 @@ WSGI_APPLICATION = 'DevNest.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
