c453a13832b3348989e68d478dd4ac9984e3501c
Mohammed Alqadda <MohammedFAlqadda@gmail.com>
2026-05-04 03:28:44 +0300

    try to deploy

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-cd DevNest
-python manage.py collectstatic --noinput
-python manage.py migrate
